565d52527e9b50c0060cd654bfa99b6a2a503282
izzywizzy <ceziakonwa@gmail.com>
2022-09-24 17:59:43 +0100

    Create randompayer.js
    random lunch payer

---
+++ randompayer.js
@@ -0,0 +1,17 @@
+function whosPaying(names) {
+
+  /******Don't change the code above*******/
+
+  //Write your code here.
+  var names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+  var payer = names[Math.floor(Math.random() * names.length)];
+
+  console.log( payer + " is going to buy lunch today")
+
+
+
+
+
+  /******Don't change the code below*******/
+}
+whosPaying()
