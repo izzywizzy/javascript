0d2f13af85e1f9092d6bb39218f23882908dba4e
izzychuks <ceziakonwa@gmail.com>
2022-09-25 21:07:07 +0100

    add new stuff

---
+++ exercises.js
@@ -0,0 +1,27 @@
+// Create a function that takes the age in years and returns the age in days.
+
+function calcAge(age) {
+	return age*365;
+}
+
+//Write a function that takes an integer minutes and converts it to seconds.
+
+function calsecs(minutes) {
+
+    return minutes * 60 ;
+    
+}
+
+// Create a function that takes voltage and current and returns the calculated power.
+
+function calPower(voltage,current){
+    return power = voltage * current ;
+}
+calPower(23,10);
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+
+function lessThan100(a, b) {
+	if ((a+b)<100){
+        return true
+    }else {return false}
+}
